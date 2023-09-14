cdffa4db56a27c4135feff8069d189a37b1b6101
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 15:04:58 +0400

    m4LoopT3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,27 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
 
+    public void printDigitsSum(int number) {
+        // Convert the number to a string
+        String numStr = Integer.toString(number);
+
+        // Initialize a variable to store the sum
+        int sum = 0;
+
+        // Iterate through the characters of the string
+        for (int i = 0; i < numStr.length(); i++) {
+            // Get the character at the current position
+            char digitChar = numStr.charAt(i);
+
+            // Convert the character to an integer by subtracting '0'
+            int digit = digitChar - '0';
+
+            // Add the digit to the sum
+            sum += digit;
+        }
+
+        // Print the sum
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int Temp, GCD = 0;
+        while (second != 0) {
+            Temp = second;
+            second = first % second;
+            first = Temp;
+        }
+        GCD = first;
+        System.out.println(GCD);
     }
 }
